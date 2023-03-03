9909ebb10e692d4d87c18489974c102d41db7462
Chris Lee <cl114@illinois.edu>
2023-03-03 13:18:46 -0600

    no client ip in constructor

--- src/checker/PCheckerFaster.ts
+++ src/checker/PCheckerFaster.ts
@@ -17,7 +17,7 @@ type ProxyOptions = {
   headers?: any;
 };
 
-type Error = {
+type ProxyError = {
   error: ENUM_ERRORS;
 };
 
@@ -33,7 +33,7 @@ export class PCheckerFast {
   public port_: string;
   public timeout_: number;
   public options_: ProxyOptions;
-  private publicIPAddress_: string;
+  private publicIPAddress_: string | Promise<string | ProxyError>;
   private timeoutsArray_: Array<any>;
   private auth_: string;
 
@@ -43,7 +43,7 @@ export class PCheckerFast {
     host: string,
     port: string,
     timeout: string,
-    publicIPAddress: string, // todo: make this optional. If not passed, get client ip with getPIP
+    publicIPAddress?: string | Promise<string | ProxyError>,
     username?: string,
     password?: string
   ) {
@@ -52,7 +52,42 @@ export class PCheckerFast {
     this.timeout_ = Number(timeout);
     this.options_ = {} as ProxyOptions;
     this.timeoutsArray_ = [];
-    this.publicIPAddress_ = publicIPAddress;
+
+    // this is poop poop and ugly
+    if (publicIPAddress !== undefined) {
+      this.publicIPAddress_ = publicIPAddress; // optional parameter, getPIP() will be called if not passed in, readiness design pattern: puttnig a promise in the object
+    }
+    if (publicIPAddress === undefined) {
+      const timeoutPromise: Promise<string> = this.createTimeout();
+      const pipPromise: Promise<string | ProxyError> = new Promise(
+        (resolve, reject) => {
+          http.get(
+            { host: "api.ipify.org", port: 80, path: "/" },
+            function (resp) {
+              resp.on("data", (ip) => {
+                resolve(String(ip));
+              });
+
+              resp.on("error", (err) => {
+                resolve({ error: ENUM_ERRORS.ConnectionError } as ProxyError);
+              });
+            }
+          );
+        }
+      );
+
+      // abiding readiness pattern, returning a promise
+      // not awaiting promise here will need to handle this in run()
+      try {
+        this.publicIPAddress_ = Promise.race([pipPromise, timeoutPromise]);
+      } catch (error) {
+        this.publicIPAddress_ = new Promise((resolve) => {
+          resolve({ error: ENUM_ERRORS.PromiseRaceError } as ProxyError);
+        });
+      }
+    }
+
+    // support for auth
     this.auth_ =
       "Basic " + Buffer.from(username + ":" + password).toString("base64");
 
@@ -63,7 +98,7 @@ export class PCheckerFast {
       path: PCheckerFast.kProxyJudgeURL,
       headers: {
         "Proxy-Authorization": this.auth_,
-      },
+      }, // todo: seperate headers from options for more control on what we are sending to proxy server
     };
   }
 
@@ -72,16 +107,15 @@ export class PCheckerFast {
    * @returns Promise<ProxyInfo | Error>
    * connects to proxy judge through http proxy, strips and scans response headers, checks time to connect
    */
-  public async checkHTTPProxy(): Promise<ProxyInfo | Error> {
-    const timeoutPromise: Promise<ProxyInfo> = new Promise((resolve) =>
-      setTimeout(() => resolve({} as ProxyInfo), this.timeout_)
-    );
-    this.timeoutsArray_.push(timeoutPromise);
+  public async checkHTTPProxy(): Promise<ProxyInfo | ProxyError> {
+    const timeoutPromise: Promise<ProxyInfo> = this.createTimeout();
+    // kind slow, difference between response time of proxy connection and runtime is signficant if client ip address is not passed into constructor
+    let resolvedPIP = await this.publicIPAddress_; 
 
-    const response: Promise<ProxyInfo | Error> = new Promise(
+    const response: Promise<ProxyInfo | ProxyError> = new Promise(
       (resolve, reject) => {
         let httpRequest = {} as ProxyInfo;
-        let errorObject = {} as Error;
+        let errorObject = {} as ProxyError;
         let startTime = new Date().getTime();
 
         http.get(this.options_, (res) => {
@@ -110,18 +144,18 @@ export class PCheckerFast {
               Object.keys(httpRequest.header).forEach(async (key) => {
                 if (key in ENUM_FlaggedHeaderValues) {
                   if (
-                    this.publicIPAddress_ !== undefined ||
-                    this.publicIPAddress_ !== ({} as any)
+                    resolvedPIP !== undefined ||
+                    resolvedPIP !== ({} as any)
                   ) {
                     if (
                       String(httpRequest.header[key as keyof JSON]) ===
-                      String(this.publicIPAddress_)
+                      String(resolvedPIP)
                     ) {
                       pipCount++;
                     }
                   } else if (
-                    Object.keys(this.publicIPAddress_).length === 0 &&
-                    this.publicIPAddress_.constructor === Object
+                    Object.keys(resolvedPIP).length === 0 &&
+                    resolvedPIP.constructor === Object
                   ) {
                     httpRequest.anonymity = undefined;
                   }
@@ -160,8 +194,18 @@ export class PCheckerFast {
       return await Promise.race([timeoutPromise, response]);
     } catch (error) {
       console.log(`httpRequest PromiseRace Error: ${error}`);
-      return { error: ENUM_ERRORS.PromiseRaceError } as Error;
+      return { error: ENUM_ERRORS.PromiseRaceError } as ProxyError;
     }
+  }
+
+  // function creates timeout, mem is managed by clearTimeouts()
+  private createTimeout<T>() {
+    const timeoutPromise: Promise<T> = new Promise((resolve) =>
+      setTimeout(() => resolve({} as T), this.timeout_)
+    );
+    this.timeoutsArray_.push(timeoutPromise);
+
+    return timeoutPromise;
   }
 
   // mem management
