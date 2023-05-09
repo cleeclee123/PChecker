e44e5a9ed639c1846a4ed37b807b7c23d8cb25b4
Chris Lee <cl114@illinois.edu>
2023-05-09 17:56:38 -0500

    adding node test runner to pchecker

---
+++ PChecker/src/tests/tests.ts
@@ -0,0 +1,12 @@
+import test from "node:test";
+import assert from "node:assert";
+
+test("synchronous passing test", (t) => {
+  // This test passes because it does not throw an exception.
+  assert.strictEqual(1, 1);
+});
+
+test("synchronous passing test", (t) => {
+  // This test passes because it does not throw an exception.
+  assert.strictEqual(2, 2);
+});
